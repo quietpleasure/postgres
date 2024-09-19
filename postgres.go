6991ec0e8926c38b5011c5c7df3789f1acc01bf7
Triod Android <triod_android@inbox.ru>
2024-09-19 17:44:34 +0300

    _

--- postgres.go
+++ postgres.go
@@ -107,7 +107,7 @@ func New(ctx context.Context, opts ...Option) (*pgxpool.Pool, error) {
 	if opt.logger != nil {
 		conCfg.ConnConfig.Tracer = &tracelog.TraceLog{
 			Logger:   opt.logger,
-			LogLevel: tracelog.LogLevelNone,
+			LogLevel: tracelog.LogLevelDebug,
 		}
 	}
 	if opt.maxconns != nil && *opt.maxconns != 0 {
